7dae9f33d8ce20f2f4aa5e4f6a27d7655dd7e30e
winbutlose <winbutlose@gmail.com>
2017-07-04 16:53:54 -0400

    test

--- MapsActivity.java
+++ MapsActivity.java
@@ -83,6 +83,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .strokeWidth(2)
                 .radius(250); // In meters
         //make a radius around you :) this one is the clickable radius
+        //jhgfjghfjgh
         CircleOptions circleClickable = new CircleOptions()
                 .center(whereuare)
                 .fillColor(Color.argb(130,0,77,64))
@@ -100,3 +101,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
  https://maps.googleapis.com/maps/api/staticmap?key=YOUR_API_KEY&center=-34.03145298593056,151.03033592700953&zoom=16&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:landscape.man_made%7Celement:labels.icon%7Cvisibility:off&style=feature:landscape.man_made%7Celement:labels.text%7Cvisibility:off&style=feature:landscape.man_made%7Celement:labels.text.fill%7Cvisibility:off&style=feature:landscape.man_made%7Celement:labels.text.stroke%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:geometry.fill%7Ccolor:0x006264&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=480x360
 
  **/
+
+//kl;kl;
